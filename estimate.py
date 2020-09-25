b9cecd3b2a4d78d9724705058a254b28b056af7c
Vrakkaris <54835829+Vrakkaris@users.noreply.github.com>
2020-09-25 16:34:16 +0000

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,4 +1,5 @@
 import math
+from numpy import random
 
 class Estimator:
 
@@ -25,6 +26,44 @@ class InformedEstimator(Estimator):
         return
 
     def get_giveaway(self, gates):
-        # You implement this.
-        # Estimate the future giveaway for the partially filled boxes at the gates.
-        return 0
+        giveaway = 0
+
+        # Simple heuristic
+        # for i in range(self.num_gates):
+        #     current_gate_weight = gates[i]
+        #
+        #     while current_gate_weight < self.capacity:
+        #         next_weight = random.normal(self.avg, self.std)
+        #         current_gate_weight += next_weight
+        #
+        #     giveaway += current_gate_weight - self.capacity
+
+        # Sample for each remaining gate and fit the largest samples into the most empty boxes.
+        # Repeat until all boxes are full.
+        open_gates = self.num_gates
+        sorted_gates = sorted(gates)
+
+        while open_gates > 0:
+            next_weight_samples = random.normal(self.avg, self.std, open_gates)
+            sorted_samples = sorted(next_weight_samples, reverse=True)
+
+            for i in range(open_gates):
+                sorted_gates[i] += sorted_samples[i]
+                if sorted_gates[i] >= self.capacity:
+                    giveaway += sorted_gates[i] - self.capacity
+
+            sorted_gates = [gate for gate in sorted_gates if gate < self.capacity]
+            sorted_gates = sorted(sorted_gates)
+            open_gates = len(sorted_gates)
+
+        # Just look at the overall remaining capacity and not at the distribution between different boxes.
+        # max_capacity = self.num_gates * self.capacity
+        # used_capacity = sum(gates)
+        #
+        # while used_capacity < max_capacity:
+        #     next_weight = random.normal(self.avg, self.std)
+        #     used_capacity += next_weight
+        #
+        # giveaway = used_capacity - max_capacity
+
+        return giveaway
