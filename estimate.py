d9b46f30dace86cabee204f195bafce010c2ad5d
Vrakkaris <54835829+Vrakkaris@users.noreply.github.com>
2020-09-25 17:44:15 +0000

    Put heuristics in separate functions

--- estimate.py
+++ estimate.py
@@ -1,9 +1,10 @@
 import math
 from numpy import random
 
+
 class Estimator:
 
-    def __init__(self, num_gates, capacity, avg, std ):
+    def __init__(self, num_gates, capacity, avg, std):
         assert (num_gates > 0)
         self.num_gates = num_gates
         self.capacity = capacity
@@ -13,6 +14,7 @@ class Estimator:
     def get_giveaway(self, gates):
         # Estimate the future giveaway for the partially filled boxes at the gates.
         return 0
+
 
 class InformedEstimator(Estimator):
 
@@ -28,18 +30,34 @@ class InformedEstimator(Estimator):
     def get_giveaway(self, gates):
         giveaway = 0
 
-        # Simple heuristic
-        # for i in range(self.num_gates):
-        #     current_gate_weight = gates[i]
-        #
-        #     while current_gate_weight < self.capacity:
-        #         next_weight = random.normal(self.avg, self.std)
-        #         current_gate_weight += next_weight
-        #
-        #     giveaway += current_gate_weight - self.capacity
+        # giveaway = self.simple_heuristic(gates)
 
-        # Sample for each remaining gate and fit the largest samples into the most empty boxes.
-        # Repeat until all boxes are full.
+        giveaway = self.largest_sample_to_largest_box(gates)
+
+        # giveaway = self.overall_capacity(gates)
+
+        return giveaway
+
+    # Iterate through the gates and fill them with the samples taken from the normal distribution
+    def simple_heuristic(self, gates):
+        giveaway = 0
+
+        for i in range(self.num_gates):
+            current_gate_weight = gates[i]
+
+            while current_gate_weight < self.capacity:
+                next_weight = random.normal(self.avg, self.std)
+                current_gate_weight += next_weight
+
+            giveaway += current_gate_weight - self.capacity
+
+        return giveaway
+
+    # Sample for each remaining gate and fit the largest samples into the most empty boxes.
+    # Repeat until all boxes are full.
+    def largest_sample_to_largest_box(self, gates):
+        giveaway = 0
+
         open_gates = self.num_gates
         sorted_gates = sorted(gates)
 
@@ -56,14 +74,17 @@ class InformedEstimator(Estimator):
             sorted_gates = sorted(sorted_gates)
             open_gates = len(sorted_gates)
 
-        # Just look at the overall remaining capacity and not at the distribution between different boxes.
-        # max_capacity = self.num_gates * self.capacity
-        # used_capacity = sum(gates)
-        #
-        # while used_capacity < max_capacity:
-        #     next_weight = random.normal(self.avg, self.std)
-        #     used_capacity += next_weight
-        #
-        # giveaway = used_capacity - max_capacity
+        return giveaway
+
+    # Just look at the overall remaining capacity and not at the distribution between different boxes.
+    def overall_capacity(self, gates):
+        max_capacity = self.num_gates * self.capacity
+        used_capacity = sum(gates)
+
+        while used_capacity < max_capacity:
+            next_weight = random.normal(self.avg, self.std)
+            used_capacity += next_weight
+
+        giveaway = used_capacity - max_capacity
 
         return giveaway
